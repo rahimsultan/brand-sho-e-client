a9d2741ba10070a7d62b94a093d3d3d0253fbee9
rahimsultan <sulrahim3@gmail.com>
2023-10-21 09:18:23 +0600

    input field type change

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -105,7 +105,7 @@ import useAuth from '../../Hooks/useAuth';
                 <div className="mt-2">
                   <input
                     className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    type="text"
+                    type="password"
                     placeholder="Password"
                     name='password'
                   ></input>
